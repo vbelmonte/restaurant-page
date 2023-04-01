257991a9eba76392f9bf9f89d8d39a4f509e3ed8
Villy Belmonte <vjbbelmont@gmail.com>
2023-03-31 17:28:00 -0700

    Implement function to minimize header and navigation

--- src/index.js
+++ src/index.js
@@ -7,19 +7,17 @@ import { createContactHeadline, createContactSection } from './contact';
 (function runPage() {
   /** MINIMIZE NAVBAR ON SCROLL * */
 
-  /* function scrollFunction() {
+  function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-      document.getElementById('navbar').style.padding = '30px 10px';
-      document.getElementById('logo').style.fontSize = '25px';
+      document.getElementsByClassName('header-logo')[0].style.height = '4rem';
     } else {
-      document.getElementById('navbar').style.padding = '80px 10px';
-      document.getElementById('logo').style.fontSize = '35px';
+      document.getElementsByClassName('header-logo')[0].style.height = '8rem';
     }
   }
 
   window.onscroll = function () {
     scrollFunction();
-  }; */
+  };
 
   function clearContents() {
     document.getElementById('content').innerHTML = '';
