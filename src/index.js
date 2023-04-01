dab47d3aa32128695c528b1b430f4dff840905e0
Villy Belmonte <vjbbelmont@gmail.com>
2023-03-31 17:14:37 -0700

    Modify header size and logo sizes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,22 @@ import { createAboutHeadline, createAboutSection } from './about';
 import { createContactHeadline, createContactSection } from './contact';
 
 (function runPage() {
+  /** MINIMIZE NAVBAR ON SCROLL * */
+
+  /* function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+      document.getElementById('navbar').style.padding = '30px 10px';
+      document.getElementById('logo').style.fontSize = '25px';
+    } else {
+      document.getElementById('navbar').style.padding = '80px 10px';
+      document.getElementById('logo').style.fontSize = '35px';
+    }
+  }
+
+  window.onscroll = function () {
+    scrollFunction();
+  }; */
+
   function clearContents() {
     document.getElementById('content').innerHTML = '';
   }
