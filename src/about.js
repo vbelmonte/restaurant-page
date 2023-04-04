e7aa5d3146ebb41916caa9c89d5e7915d6611883
Villy Belmonte <vjbbelmont@gmail.com>
2023-04-03 18:20:44 -0700

    Add link to The Odin Project on About page

--- src/about.js
+++ src/about.js
@@ -20,7 +20,7 @@ function createAboutSection() {
 
   h2.innerHTML = 'Sharing our love for Neopolitan food<br><br>';
 
-  p1.innerHTML = 'This website is a project of The Odin Project and is part of the Full Stack Javascript curriculum. The purpose of this website is to demonstrate knowledge and skills of Classes, ES6 Modules, and Webpack.<br><br>';
+  p1.innerHTML = 'This website is a project of <a href=\'https://www.theodinproject.com\' target=\'_blank\'>The Odin Project</a> and is part of the Full Stack Javascript curriculum. The purpose of this website is to demonstrate knowledge and skills of Classes, ES6 Modules, and Webpack.<br><br>';
 
   p2.innerHTML = 'The restaurant described in this website is fictional and inspired by real restaurants that serve authentic Italian dishes. The names of the dishes mentioned in this site are not intended to be an accurate representation of Itailan food, namely Neopolitan dishes.';
 
